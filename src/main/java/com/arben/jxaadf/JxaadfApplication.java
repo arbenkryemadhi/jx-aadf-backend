c5cabc43f4bc28192f8939e6623e9586b888c2a1
Arben Kryemadhi <arbenofc@gmail.com>
2025-04-30 23:42:17 +0200

    a

---
+++ src/main/java/com/arben/jxaadf/JxaadfApplication.java
@@ -0,0 +1,13 @@
+package com.arben.jxaadf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxaadfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxaadfApplication.class, args);
+	}
+
+}
