ea6d290ffacd1efd01f7bc6f3ff7e9cb7c1ba0ca
Arben Kryemadhi <arbenofc@gmail.com>
2025-05-01 22:31:40 +0200

    a

--- src/main/java/com/arben/jxaadf/appuser/AppUserController.java
+++ src/main/java/com/arben/jxaadf/appuser/AppUserController.java
@@ -1,13 +1,13 @@
 package com.arben.jxaadf.appuser;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/appuser")
 public class AppUserController {
